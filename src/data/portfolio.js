b9f7a8f517bfca9243933979c58f7fd0959f95d6
Amrit Pandher <amrit.pandher@bellevuecollege.edu>
2023-03-30 18:37:38 -0700

    Portfolio component set up and initial example entries

---
+++ src/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        title: 'Avian Influenza CDC Dataset Analysis',
+        imgUrl: '',
+        stack: ['SQL', 'PowerBI', 'Excel'],
+        link: 'https://https://github.com/amritspandher',
+    },
+    {
+        title: 'Automated Greenhouse Capstone',
+        imgUrl: '',
+        stack: ['Kotlin', 'Java', 'SQL', 'AWS', 'Grafana', 'Arduino IDE'],
+        link: 'https://https://github.com/amritspandher',
+    },
+    {
+        title: 'Community Transit Java API',
+        imgUrl: '',
+        stack: ['Java'],
+        link: 'https://https://github.com/amritspandher',
+    }
+]
